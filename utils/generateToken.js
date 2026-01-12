1502367ab9a980e7b2de496f8908cfe9e2d44163
NestorGodoyJr <nestorgodoyjr@gmail.com>
2026-01-12 20:20:02 +0800

    fix(auth): handle missing JWT_SECRET with proper status code

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,14 +4,14 @@ import CustomError from './CustomError.js'
 const JWT_SECRET = process.env.JWT_SECRET
 
 export const generateAccessToken = (id) =>{
-    if( !JWT_SECRET ) throw new CustomError('JWT_Secret undefined!')
+    if( !JWT_SECRET ) throw new CustomError('JWT_Secret undefined!', 400)
     return jwt.sign({id}, JWT_SECRET, {
         expiresIn: '15m'
     })
 }
 
 export const generateRefreshToken = (id) =>{
-    if( !JWT_SECRET ) throw new CustomError('JWT_Secret undefined!')
+    if( !JWT_SECRET ) throw new CustomError('JWT_Secret undefined!', 400)
     return jwt.sign({id}, JWT_SECRET, {
         expiresIn: '7d'
     })
