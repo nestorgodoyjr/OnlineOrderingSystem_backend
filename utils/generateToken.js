d8c511bedd9db38ae80523d5c750b7f4def8c78d
NestorGodoyJr <nestorgodoyjr@gmail.com>
2026-01-13 02:35:44 +0800

    fix dotenv, it's outside the functions

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,9 +1,8 @@
 import jwt from 'jsonwebtoken'
 import CustomError from './CustomError.js'
 
-const JWT_SECRET = process.env.JWT_SECRET
-
 export const generateAccessToken = (id) =>{
+    const JWT_SECRET = process.env.JWT_SECRET
     if( !JWT_SECRET ) throw new CustomError('JWT_Secret undefined!', 400)
     return jwt.sign({id}, JWT_SECRET, {
         expiresIn: '15m'
@@ -11,6 +10,7 @@ export const generateAccessToken = (id) =>{
 }
 
 export const generateRefreshToken = (id) =>{
+    const JWT_SECRET = process.env.JWT_SECRET
     if( !JWT_SECRET ) throw new CustomError('JWT_Secret undefined!', 400)
     return jwt.sign({id}, JWT_SECRET, {
         expiresIn: '7d'
